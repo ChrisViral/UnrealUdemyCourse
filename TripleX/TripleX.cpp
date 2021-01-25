156d6194076d0d8d34a22ccab1ba2e0822290d68
Christophe Savard <christophe_savard@hotmail.ca>
2021-01-25 17:26:07 -0500

    Fixing gitignore

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -1,5 +1,15 @@
+#include <iostream>
 
 int main()
 {
+	try
+	{
+		std::cout << "You are a secret agent breaking into a secure server room" << std::endl;
+		std::cout << "Enter the secret code to continue..." << std::endl;
+	}
+	catch (...)
+	{
+		return 1;
+	}
     return 0;
 }
